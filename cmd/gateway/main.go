591c7c181c0ebaa7eac5c2d5cd51efdb88fdc3b6
deface90 <deface06@yandex.ru>
2022-03-01 12:54:42 +0500

    initial commit

---
+++ cmd/gateway/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/def-stocks-t/getaway/internal/config"
+	"github.com/def-stocks-t/getaway/internal/rest"
+	"github.com/deface90/go-logger/filename"
+	"github.com/jinzhu/configor"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	logger := log.New()
+
+	filenameHook := filename.NewHook()
+	filenameHook.Field = "line"
+	logger.AddHook(filenameHook)
+
+	var conf config.Config
+	err := configor.Load(&conf, "config.json")
+	if err != nil {
+		log.Errorf("Failed to read config.json, using default config values")
+	}
+
+	restService := rest.NewRestService(conf, logger)
+	restService.Run()
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/def-stocks-t/getaway
+
+go 1.17
+
+require (
+	github.com/deface90/go-logger v0.8.0
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/render v1.0.1
+	github.com/jinzhu/configor v1.2.1
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.0
+)
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
